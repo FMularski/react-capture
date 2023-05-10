d6be6ab1b22210db0274cd03afd71d524c68ad58
Filip <filipelblag@gmail.com>
2023-05-10 09:42:57 +0200

    about us children animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,20 +1,14 @@
 import home1 from '../img/home1.png';
 import {About, Description, Hide, Image} from '../styles';
 import {motion} from 'framer-motion';
+import { titleAnimation, fade, photoAnimation } from '../animation';
 
 const AboutSection = () => {
-    const titleAnimation = {
-        hidden: {opacity: 0},
-        show: {opacity: 1, transition: {duration: 1}}
-    };
-    const container = {
-        hidden: {x: 100},
-        show: {x: 0, transition: {duration: 1, ease: "easeOut", staggerChildren: 1, when: 'afterChildren'}}
-    }
+    
     return (
         <About>
             <Description>
-                <motion.div variants={container} initial="hidden" animate="show" className="title">
+                <motion.div>
                     <Hide>
                         <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
                     </Hide>
@@ -25,11 +19,11 @@ const AboutSection = () => {
                         <motion.h2 variants={titleAnimation}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="a man with a camera" />
+                <motion.img variants={photoAnimation} src={home1} alt="a man with a camera" />
             </Image>
         </About>
     )
